42b712916892085a3c6dd04700ff501a26a67943
Patrick <patrickz.t@outlook.com>
2023-03-06 11:40:59 -0300

    Salvando alterações de ultima hora

--- Controllers/WeaponController.cs
+++ Controllers/WeaponController.cs
@@ -1,7 +1,31 @@
+using dotNet.Dtos.Weapon;
+using dotNet.Service.WeaponService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace dotNet.Controllers
-{
-  public class WeaponController
+{     
+        [Authorize]
+        [ApiController]
+        [Route("[controller]")]
+  public class WeaponController : ControllerBase
     {
+    private readonly IWeaponService _weaponService;
+        public WeaponController(IWeaponService weaponService)
+        {
+           _weaponService = weaponService;
+            
+        }
         
-    }
+        [HttpPost]
+
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> AddWeapon(AddWeaponDto newWeapon)
+        {
+            return Ok(await _weaponService.AddWeapon(newWeapon));
+
+        }
+
+
+        
+    };
 }
